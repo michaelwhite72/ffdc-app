af26047753aa472020a3342501e7c8c4c7077752
sebastian2449 <77408993+sebastian2449@users.noreply.github.com>
2021-02-12 13:57:03 -0500

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,47 @@
+
+//Importa las vistas y los componentes asosciados a cada ruta
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+//Imports each of the pages 
+import AboutPage from '../views/AboutPage.vue';
+import BlogPage from '../views/BlogPage.vue';
+import ContactPage from '../views/ContactPage.vue';
+import HomePage from '../views/HomePage.vue';
+import ServicesPage from '../views/ServicesPage.vue';
 
 Vue.use(VueRouter)
 
+//Se construyen las rutas asociadas a cada pagina, vista y/o componente
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
-  },
-  {
+    component: HomePage,
+  }, {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: AboutPage,
+  }, {
+    path: '/services',
+    name: 'Services',
+    component: ServicesPage,
+  }, {
+    path: '/blog',
+    name: 'Blog',
+    component: BlogPage,
+  }, {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactPage,
+  },{
+    path: '/',
+    redirect: '/home',
+  }, {
+    path: '*',
+    name: 'NotFoundPage',
+    component: NotFoundPage,
+  },
 ]
 
 const router = new VueRouter({
