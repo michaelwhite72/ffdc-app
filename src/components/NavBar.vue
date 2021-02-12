f63180faac8114bde357b37a6da9a762d56ccc54
sebastian2449 <77408993+sebastian2449@users.noreply.github.com>
2021-02-12 13:56:09 -0500

    commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div id="nav-bar">
+    <router-link to="/home" id="home-link">
+        <img src="../assets" />
+    </router-link>
+
+</div>
+<!-- //header -->
+
+</template>
+
+<template>
+    <div id="nav-bar">
+        <router-link to="/products" id="products-link">
+        <h1>FABULOUS FOOTWEAR</h1>
+        </router-link>
+        <router-link to="/cart" id="cart-link">
+            <button>Shopping Cart</button>
+        </router-link>
+    </div>
+</template>
